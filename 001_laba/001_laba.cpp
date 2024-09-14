7302b9eb68b85796b9abf1db7f731c78b4167050
Dmitry Bakin <dima.bakin2017@yandex.ru>
2024-09-14 08:41:40 +0700

    Update 001_laba.cpp

--- 001_laba/001_laba.cpp
+++ 001_laba/001_laba.cpp
@@ -15,12 +15,15 @@ public:
 	int Numerator() const;
 	int Denominator() const;
 	Fraction sum(Fraction a, Fraction b);
-	//Fraction operator+(Fraction b);
+	Fraction operator+(Fraction b);
+	Fraction operator-(Fraction b);
+	Fraction operator*(Fraction b);
+	Fraction operator/(Fraction b);
 private:
 	int m_Numenator;
 	int m_Denominator;
 };
-Fraction sum(Fraction a, Fraction b)
+Fraction Fraction::sum(Fraction a, Fraction b)
 {
 	Fraction c;
 	c.SetNumerator(a.Numerator() * b.Denominator() + a.Denominator() * b.Numerator());
@@ -28,24 +31,54 @@ Fraction sum(Fraction a, Fraction b)
 	return c;
 }
 
+Fraction Fraction::operator+(Fraction b)
+{
+	Fraction c;
+	c.SetNumerator(m_Numenator * b.m_Denominator + m_Denominator * b.m_Numenator);
+	c.SetDenominator(m_Denominator * b.m_Denominator);
+	return c;
+}
+Fraction Fraction::operator-(Fraction b)
+{
+	Fraction c;
+	c.SetNumerator(m_Numenator * b.m_Denominator - m_Denominator * b.m_Numenator);
+	c.SetDenominator(m_Denominator * b.m_Denominator);
+	return c;
+}
+Fraction Fraction::operator*(Fraction b)
+{
+	Fraction c;
+	c.SetNumerator(m_Numenator * b.m_Numenator);
+	c.SetDenominator(m_Denominator * b.m_Denominator);
+	return c;
+}
+Fraction Fraction::operator/(Fraction b)
+{
+	Fraction c;
+	c.SetNumerator(m_Numenator * b.m_Denominator);
+	c.SetDenominator(m_Denominator * b.m_Numenator);
+	return c;
+}
 int main()
 {
-	Fraction a, b,c;
+	Fraction a, b, c, d, e, f;
 	int na, da, nb, db;
-	cout << "Vvedite Numerator a ";
-	cin >> na;
-	cout << "Vvedite Denominator a ";
-	cin >> da;
-	cout << "Vvedite Numerator b ";
-	cin >> nb;
-	cout << "Vvedite  Denominator b ";
-	cin >> db;
+	cout << "Vvedite Numerator a "; cin >> na;
+	cout << "Vvedite Denominator a "; cin >> da;
+	cout << "Vvedite Numerator b "; cin >> nb;
+	cout << "Vvedite  Denominator b "; cin >> db;
 	a.SetNumerator(na);
 	a.SetDenominator(da);
 	b.SetNumerator(nb);
 	b.SetDenominator(db);
-	c = sum(a, b);
-	cout << c.Numerator() << "/" << c.Denominator();
+	c = a + b;
+	d = a - b;
+	e = a * b;
+	f = a / b;
+	cout << c.Numerator() << "/" << c.Denominator() << "\n";
+	cout << d.Numerator() << "/" << d.Denominator() << "\n";
+	cout << e.Numerator() << "/" << e.Denominator() << "\n";
+	cout << f.Numerator() << "/" << f.Denominator() << "\n";
 }
 
 void Fraction::SetNumerator(int Numerator)
@@ -62,6 +95,7 @@ int Fraction::Denominator() const
 {
 	return m_Denominator;
 }
+
 int Fraction::Numerator() const
 {
 	return m_Numenator;
