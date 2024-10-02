3b45d41d360876d47111cd995d7baaabc7414da7
Dmitry Bakin <dima.bakin2017@yandex.ru>
2024-10-02 23:49:10 +0700

    completion to the end

--- 001_laba/001_laba.cpp
+++ 001_laba/001_laba.cpp
@@ -1,102 +1,249 @@
 ﻿#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <conio.h>
 #include <string.h>
-#include <Windows.h>
 using namespace std;
 #pragma once
+void swap(int& a, int& b)
+{
+	int c;
+	c = a;
+	a = b;
+	b = c;
+}
 struct Fraction
 {
 public:
+	Fraction();
+	Fraction(int Numerator, int Denominator);
 	void SetNumerator(int);
 	void SetDenominator(int);
 	int Numerator() const;
 	int Denominator() const;
-	Fraction sum(Fraction a, Fraction b);
 	Fraction operator+(Fraction b);
 	Fraction operator-(Fraction b);
 	Fraction operator*(Fraction b);
 	Fraction operator/(Fraction b);
+	bool operator>(Fraction b);
+	bool operator<(Fraction b);
+	bool operator==(Fraction b);
+	bool operator!=(Fraction b);
+	Fraction operator^(int n);
+	bool denBigger(Fraction b);
+	void fractionReduction();
+	void inputFraction();
+	void outputFraction();
 private:
-	int m_Numenator;
-	int m_Denominator;
+	int m_Numerator = 1;
+	int m_Denominator = 1;
 };
-Fraction Fraction::sum(Fraction a, Fraction b)
-{
-	Fraction c;
-	c.SetNumerator(a.Numerator() * b.Denominator() + a.Denominator() * b.Numerator());
-	c.SetDenominator(a.Denominator() * b.Denominator());
-	return c;
-}
-
 Fraction Fraction::operator+(Fraction b)
 {
 	Fraction c;
-	c.SetNumerator(m_Numenator * b.m_Denominator + m_Denominator * b.m_Numenator);
+	c.SetNumerator(m_Numerator * b.m_Denominator + m_Denominator * b.m_Numerator);
 	c.SetDenominator(m_Denominator * b.m_Denominator);
 	return c;
 }
 Fraction Fraction::operator-(Fraction b)
 {
 	Fraction c;
-	c.SetNumerator(m_Numenator * b.m_Denominator - m_Denominator * b.m_Numenator);
+	c.SetNumerator(m_Numerator * b.m_Denominator - m_Denominator * b.m_Numerator);
 	c.SetDenominator(m_Denominator * b.m_Denominator);
 	return c;
 }
 Fraction Fraction::operator*(Fraction b)
 {
 	Fraction c;
-	c.SetNumerator(m_Numenator * b.m_Numenator);
+	c.SetNumerator(m_Numerator * b.m_Numerator);
 	c.SetDenominator(m_Denominator * b.m_Denominator);
 	return c;
 }
 Fraction Fraction::operator/(Fraction b)
 {
 	Fraction c;
-	c.SetNumerator(m_Numenator * b.m_Denominator);
-	c.SetDenominator(m_Denominator * b.m_Numenator);
+	c.SetNumerator(m_Numerator * b.m_Denominator);
+	c.SetDenominator(m_Denominator * b.m_Numerator);
 	return c;
+}
+bool Fraction::operator>(Fraction b)
+{
+	if (m_Numerator * b.m_Denominator > b.m_Numerator * m_Denominator)
+		return true;
+	else
+		return false;
+}
+bool Fraction::operator<(Fraction b)
+{
+	if (m_Numerator * b.m_Denominator < b.m_Numerator * m_Denominator)
+		return true;
+	else
+		return false;
+}
+bool Fraction::operator==(Fraction b)
+{
+	if (m_Numerator * b.m_Denominator == b.m_Numerator * m_Denominator)
+		return true;
+	else
+		return false;
+}
+bool Fraction::operator!=(Fraction b)
+{
+	if (m_Numerator * b.m_Denominator != b.m_Numerator * m_Denominator)
+		return true;
+	else
+		return false;
+}
+Fraction Fraction::operator^(int n)
+{
+	Fraction b;
+	if (n > 0)
+		for (int i = 0; i < n; i++)
+		{
+			b.SetNumerator(b.m_Numerator * m_Numerator);
+			b.SetDenominator(b.m_Denominator * m_Denominator);
+		}
+	if (n < 0)
+	{
+		swap(m_Numerator, m_Denominator);
+		n = -n;
+		for (int i = 0; i < n; i++)
+		{
+			b.SetNumerator(b.m_Numerator * m_Numerator);
+			b.SetDenominator(b.m_Denominator * m_Denominator);
+		}
+	}
+	return b;
+
+}
+bool Fraction::denBigger(Fraction b) 
+{
+	if (m_Denominator > b.m_Denominator)
+		return true;
+	else
+		return false;
+}
+void Fraction::fractionReduction()
+{
+	while (true)
+	{
+		int NOD = 1;
+		for (int i = 1; i <= min(abs(m_Numerator), abs(m_Denominator)); ++i)
+			if (m_Numerator % i == 0 && m_Denominator % i == 0)
+				NOD = i;
+		if (NOD == 1)
+			break;
+		m_Numerator /= NOD;
+		m_Denominator /= NOD;
+	}
+	if (m_Numerator < 0 && m_Denominator < 0)
+	{
+		m_Numerator *= -1;
+		m_Denominator *= -1;
+	}
+}
+void Fraction::inputFraction()
+{
+	cout << "Enter numerator this fraction: ";
+	cin >> m_Numerator;
+	cout  << "Enter denominator this fraction: ";
+	cin >> m_Denominator;
+	if (m_Denominator == 0)
+	{
+		cout << "Denominator cannot be == 0" << endl;
+		m_Numerator = 0;
+		m_Denominator = 1;
+	}
+}
+void Fraction::outputFraction()
+{
+	cout << m_Numerator << "/" << m_Denominator << endl;
 }
 int main()
 {
-	Fraction a, b, c, d, e, f;
-	int na, da, nb, db;
-	cout << "Vvedite Numerator a "; cin >> na;
-	cout << "Vvedite Denominator a "; cin >> da;
-	cout << "Vvedite Numerator b "; cin >> nb;
-	cout << "Vvedite  Denominator b "; cin >> db;
-	a.SetNumerator(na);
-	a.SetDenominator(da);
-	b.SetNumerator(nb);
-	b.SetDenominator(db);
-	c = a + b;
-	d = a - b;
-	e = a * b;
-	f = a / b;
-	cout << c.Numerator() << "/" << c.Denominator() << "\n";
-	cout << d.Numerator() << "/" << d.Denominator() << "\n";
-	cout << e.Numerator() << "/" << e.Denominator() << "\n";
-	cout << f.Numerator() << "/" << f.Denominator() << "\n";
-}
+	Fraction a, b, sum, difference, product, quotient; int degree;
+	a.inputFraction();
+	a.fractionReduction();
+	a.outputFraction();
+	b.inputFraction();
+	b.fractionReduction();
+	b.outputFraction();
 
+	sum = a + b;
+	sum.fractionReduction();
+	cout << "a + b = ";
+	sum.outputFraction();
+
+	difference = a - b;
+	difference.fractionReduction();
+	cout << "a - b = ";
+	difference.outputFraction();
+
+	product = a * b;
+	product.fractionReduction();
+	cout << "a * b = ";
+	product.outputFraction();
+
+	quotient = a / b;
+	quotient.fractionReduction();
+	cout << "a / b = ";
+	quotient.outputFraction();
+
+	if (a > b)
+		cout << "a > b" << endl;
+
+	if (a < b)
+		cout << "a < b" << endl;
+
+	if (a == b)
+		cout << "a == b" << endl;
+
+	if (a != b)
+		cout << "a != b" << endl;
+
+	if (a.denBigger(b))
+		cout << "a.denominator > b.Denominator" << endl;
+	if (a.denBigger(b) == false)
+		cout << "a.denominator <= b.Denominator" << endl;
+
+	cout << "Enter the degree: ";
+	cin >> degree;
+	a = a ^ degree;
+	cout << "a ^ " << degree << " = ";
+	a.outputFraction();
+
+	return 0;
+}
+Fraction::Fraction()
+{
+}
+Fraction::Fraction(int Numerator, int Denominator)
+{
+	if (Denominator == 0)
+	{
+		cout << "Denominator cannot be == 0" << endl;
+		m_Numerator = 0;
+		m_Denominator = 1;
+	}
+	else 
+	{
+		m_Numerator = Numerator;
+		m_Denominator = Denominator;
+
+	}
+}
 void Fraction::SetNumerator(int Numerator)
 {
-	m_Numenator = Numerator;
+	m_Numerator = Numerator;
 }
-
 void Fraction::SetDenominator(int Denominator)
 {
 	m_Denominator = Denominator;
 }
-
 int Fraction::Denominator() const
 {
 	return m_Denominator;
 }
-
 int Fraction::Numerator() const
 {
-	return m_Numenator;
+	return m_Numerator;
 }
