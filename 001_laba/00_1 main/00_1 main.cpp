f15d38eb634b272ccc84228a0f442269e688e9da
Dmitry Bakin <dima.bakin2017@yandex.ru>
2024-10-10 11:10:48 +0700

    + 00_1 main.cpp

---
+++ 001_laba/00_1 main/00_1 main.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include "C:\O-O-P\001_laba\Fraction\Fraction.cpp"
+int main()
+{
+	Fraction a, b, sum, difference, product, quotient, deg;
+	int degree;
+	a.inputFraction();
+	a.fractionReduction();
+	a.outputFraction();
+	b.inputFraction();
+	b.fractionReduction();
+	b.outputFraction();
+
+	sum = a + b;
+	sum.fractionReduction();
+	std::cout << "a + b = ";
+	sum.outputFraction();
+
+	difference = a - b;
+	difference.fractionReduction();
+	std::cout << "a - b = ";
+	difference.outputFraction();
+
+	product = a * b;
+	product.fractionReduction();
+	std::cout << "a * b = ";
+	product.outputFraction();
+
+	quotient = a / b;
+	quotient.fractionReduction();
+	std::cout << "a / b = ";
+	quotient.outputFraction();
+
+	if (a > b)
+		std::cout << "a > b" << std::endl;
+
+	if (a < b)
+		std::cout << "a < b" << std::endl;
+
+	if (a == b)
+		std::cout << "a == b" << std::endl;
+
+	if (a != b)
+		std::cout << "a != b" << std::endl;
+
+	if (a.denBigger(b))
+		std::cout << "a.denominator > b.Denominator" << std::endl;
+	if (!a.denBigger(b))
+		std::cout << "a.denominator <= b.Denominator" << std::endl;
+
+	std::cout << "Enter degree a: ";
+	std::cin >> degree;
+	deg = a ^ degree;
+	std::cout << "a ^ " << degree << " = ";
+	deg.outputFraction();
+	return 0;
+}
--- 001_laba/Fraction/Fraction.cpp
+++ 001_laba/Fraction/Fraction.cpp
@@ -123,13 +123,13 @@ Fraction::Fraction(int numerator, int denominator)
 
 	}
 }
-void Fraction::setNumerator(int numerator)
+void Fraction::setNumerator(int Numerator)
 {
-	m_numerator = numerator;
+	m_numerator = Numerator;
 }
-void Fraction::setDenominator(int denominator)
+void Fraction::setDenominator(int Denominator)
 {
-	m_denominator = denominator;
+	m_denominator = Denominator;
 }
 int Fraction::denominator() const
 {
@@ -138,59 +138,4 @@ int Fraction::denominator() const
 int Fraction::numerator() const
 {
 	return m_numerator;
-}
-int main()
-{
-	Fraction a, b, sum, difference, product, quotient, deg;
-	int degree;
-	a.inputFraction();
-	a.fractionReduction();
-	a.outputFraction();
-	b.inputFraction();
-	b.fractionReduction();
-	b.outputFraction();
-
-	sum = a + b;
-	sum.fractionReduction();
-	std::cout << "a + b = ";
-	sum.outputFraction();
-
-	difference = a - b;
-	difference.fractionReduction();
-	std::cout << "a - b = ";
-	difference.outputFraction();
-
-	product = a * b;
-	product.fractionReduction();
-	std::cout << "a * b = ";
-	product.outputFraction();
-
-	quotient = a / b;
-	quotient.fractionReduction();
-	std::cout << "a / b = ";
-	quotient.outputFraction();
-
-	if (a > b)
-		std::cout << "a > b" << std::endl;
-
-	if (a < b)
-		std::cout << "a < b" << std::endl;
-
-	if (a == b)
-		std::cout << "a == b" << std::endl;
-
-	if (a != b)
-		std::cout << "a != b" << std::endl;
-
-	if (a.denBigger(b))
-		std::cout << "a.denominator > b.Denominator" << std::endl;
-	if (!a.denBigger(b))
-		std::cout << "a.denominator <= b.Denominator" << std::endl;
-
-	std::cout << "Enter degree a: ";
-	std::cin >> degree;
-	deg = a ^ degree;
-	std::cout << "a ^ " << degree << " = ";
-	deg.outputFraction();
-	return 0;
 }
